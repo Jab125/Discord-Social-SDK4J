d5fd9c905f0b38e9e020073a690dbf65d42cd7e4
Jab125 <67534807+Jab125@users.noreply.github.com>
2025-08-24 23:28:32 +0000

    Add license headers

--- src/main/java/dev/jab125/discordsocialsdk/pp/PointerWrapper.java
+++ src/main/java/dev/jab125/discordsocialsdk/pp/PointerWrapper.java
@@ -1,3 +1,8 @@
+// Copyright 2025 Jab125
+// This file is part of Discord-Social-SDK4J.
+// Discord-Social-SDK4J is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+// Discord-Social-SDK4J is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+// You should have received a copy of the GNU Lesser General Public License along with Discord-Social-SDK4J. If not, see <https://www.gnu.org/licenses/>.
 package dev.jab125.discordsocialsdk.pp;
 
 import java.lang.foreign.MemorySegment;
